e2aee712d9938df1668336fd80fab788b842ca5e
subhajyoti-prusty <subhajyoti.2004@gmail.com>
2025-07-13 20:13:53 +0530

    Backend Code Setup

---
+++ Restaurant_BE/src/test/java/com/my/restaurant/RestaurantBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
