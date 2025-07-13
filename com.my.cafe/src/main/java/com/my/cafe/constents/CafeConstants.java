156369ddce9801702766bffc4fa6a21ad506177c
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2025-07-13 14:26:31 +0530

    Delete com.my.project

--- com.my.cafe/src/main/java/com/my/cafe/constents/CafeConstants.java
+++
@@ -1,9 +0,0 @@
-package com.my.cafe.constents;
-
-public class CafeConstants {
-
-    public static final String SOMETHING_WENT_WRONG = "Something Went Wrong.";
-
-    public static final String INVALID_DATA = "Invalid Data.";
-
-}
