4e8f497d764c388aad09e3183f141c4c8021fd4b
subhajyoti-prusty <subhajyoti.2004@gmail.com>
2025-07-27 00:03:52 +0530

    Login API

--- Restaurant_BE/src/main/java/com/my/restaurant/config/WebSecurityConfiguration.java
+++ Restaurant_BE/src/main/java/com/my/restaurant/config/WebSecurityConfiguration.java
@@ -3,6 +3,8 @@ package com.my.restaurant.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -30,5 +32,10 @@ public class WebSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
